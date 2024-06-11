cb6f66843b5da0027aac4de6e58928f4f2232364
pacflypy <pacflypy@outlook.com>
2024-06-11 23:57:59 +0200

    update

--- src/pacflypy/flypy/lexer/lexer.py
+++ src/pacflypy/flypy/lexer/lexer.py
@@ -1,30 +1,9 @@
-# The flypy Lexer for Working with flypy Extensions
-import pacflypy.command as command
-import pacflypy.string as string
-import pacflypy.system as system
 import re
-import shutil
-
-content = """
-create pyclass Example
-    classfunc init(self, name -> str <- None)
-        pyvar -> self.name = name
-
-    classfunc write_name(self)
-        pyshow str(#_self.name_#)
-
-pyfunc add_sum_as_string(a -> int <- 0, b -> int <- 0)
-    give_back str(#_a_# + #_b_#)
-
-/// * This is a Doc String for flypy
-/// * flypy should a Extension Language for Python in Combination with pacflypy
-/// * In there You can Write Python Classes and Function Easier and Can Bind him In
-/// * The flypy Extension Language
-"""
+import pacflypy
 
 class _TOKEN:
     """
-    Class for Token with all Accepted Token
+    Klasse für Token mit allen akzeptierten Token
     """
     TOKEN = {
         'CLASS': r'\bpyclass\b',
@@ -37,30 +16,29 @@ class _TOKEN:
         'INT': r'\bint\b',
         'NONE': r'\bnone\b',
         'DOC': r'\b///\*',
-        '': r'\b\b',
+        'COMMENT': r'///.*',
+        'IDENTIFIER': r'\b[a-zA-Z_][a-zA-Z0-9_]*\b',
+        'OPERATOR': r'->|<-',
+        'NUMBER': r'\b\d+\b',
+        'NEWLINE': r'\n',
+        'WHITESPACE': r'\s+',
+        'UNKNOWN': r'.'
     }
 
 class lexer:
     """
-    Class for Lexing the flypy Extension Language
+    Klasse zum Lexen der flypy-Erweiterungssprache
     """
-    def __init__(self, content: str) -> dict:
-        """
-        Init the Lexer with Given Content
-        """
+    def __init__(self, content: str):
         self.content = content
-        token = self._tokenize(content=self.content)
-        return token
+        self.tokens = self._tokenize(content=self.content)
 
     def _tokenize(self, content: str) -> dict:
-        """
-        Tokenize the Content and Return A Directory with all Tokens
-        """
-        # Erstelle ein Dictionary fuer die Token
-        token = {}
-
-        # Werte content aus und Parse diese zu Token
+        token_dict = {}
         for token_type, regex in _TOKEN.TOKEN.items():
-            matches = re.finditer(regex, content)
-            token[token_type] = [match.group(0) for match in matches]
-        return token
+            matches = re.finditer(regex, content, re.MULTILINE)
+            token_dict[token_type] = [match.group(0) for match in matches]
+        return token_dict
+
+    def get(self):
+        return self.tokens
