15f061c93882e827ce0dd84463ea5d7664b9c03f
pacflypy <pacflypy@outlook.com>
2024-06-11 15:14:40 +0200

    update

--- src/pacflypy/flypy/lexer/lexer.py
+++ src/pacflypy/flypy/lexer/lexer.py
@@ -1,4 +1,9 @@
 # The flypy Lexer for Working with flypy Extensions
+import pacflypy.command as command
+import pacflypy.string as string
+import pacflypy.system as system
+import re
+import shutil
 
 content = """
 create pyclass Example
@@ -6,8 +11,29 @@ create pyclass Example
         pyvar -> self.name = name
 
     classfunc write_name(self)
-        give_back str(#_self.name_#)
+        pyshow str(#_self.name_#)
 
 pyfunc add_sum_as_string(a -> int <- 0, b -> int <- 0)
     give_back str(#_a_# + #_b_#)
+
+/// * This is a Doc String for flypy
+/// * flypy should a Extension Language for Python in Combination with pacflypy
+/// * In there You can Write Python Classes and Function Easier and Can Bind him In
+/// * The flypy Extension Language
 """
+
+class lexer:
+    """
+    Class for Lexing the flypy Extension Language
+    """
+    def __init__(self, content: str) -> str:
+        """
+        Init the Lexer with Given Content
+        """
+        self.content = content
+        token = self._tokenize(content=self.content)
+
+    def _tokenize(self, content: str) -> dict:
+        """
+        Tokenize the Content and Return A Directory with all Tokens
+        """
